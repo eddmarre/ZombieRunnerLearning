fd67a6808fa61338984d3ce69d167298f3645743
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-13 15:04:35 -0400

    GameOver & DeathHandler
    +Game Over Scene with 2 buttons
    +Death Handler for player death

---
+++ Assets/Scripts/DeathHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHandler : MonoBehaviour
+{
+    [SerializeField] Canvas gameOverCanvas;
+    void Start()
+    {
+        gameOverCanvas.enabled = false;
+    }
+
+    public void HandleDeath()
+    {
+        GetComponentInChildren<Weapon>().enabled=false;
+        gameOverCanvas.enabled = true;
+        //unlock cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        //stop game time to prevent conflicts
+        Time.timeScale = 0;
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour
         playerHealth -= damage;
         if (playerHealth <= 0)
         {
-            print("dead");//todo remove later
+            GetComponent<DeathHandler>().HandleDeath();
         }
     }
 
