aa08fe927e6ef9d3e102fe360c8bbddc4321dd25
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-13 15:42:50 -0400

    Added Toggle Zoom
    +toggle Zoom with gun

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField]  float regularFOV = 60.0f;
+    [SerializeField] float ADSFOV = 45.0f;
+    [SerializeField] Camera FPSCamera;
+    bool zoomToggle=false;
+    void Update()
+    {
+        CameraFOV();
+    }
+    void CameraFOV()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if(zoomToggle==false)
+            {
+                zoomToggle=true;
+                FPSCamera.fieldOfView=ADSFOV;
+            }
+            else
+            {
+                zoomToggle=false;
+                FPSCamera.fieldOfView=regularFOV;
+            }
+        }
+    }
+}
