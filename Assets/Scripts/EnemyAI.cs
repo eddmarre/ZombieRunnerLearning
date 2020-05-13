1174448ac314a485431fdaa502954a368ecb096e
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-13 10:53:48 -0400

    Update EnemyAI.cs
    +Enemy AI smarter
    +Once enemy is triggered it will always follow player
    +when close enough enemy will stop to attack

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,24 +8,41 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] float chaseRange = 5f;
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;//float initializes at 0 so doing this prevents issue at start time
-    
-
+    bool isProvoked = false;
+    float enemyStoppingDistance = 2f;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        
     }
 
 
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget <= chaseRange)
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
         }
 
-
     }
+    void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            print("chasing");//todo delete later
+            chaseTarget();
+        }
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
     /// <summary>
     /// Callback to draw gizmos only if the object is selected.
     /// Lets us see the range of the selected object
@@ -34,5 +51,14 @@ public class EnemyAI : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+    void chaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+        navMeshAgent.stoppingDistance = enemyStoppingDistance;
+    }
+    void AttackTarget()
+    {
+        Debug.Log(name + "Attacking" + target.name);//todo delete later
     }
 }
