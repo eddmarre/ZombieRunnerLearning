199d238d089d4ae4ad69c322bf78f47707fafbc4
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-14 10:23:06 -0400

    Added Ammo Pickups

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -21,13 +21,18 @@ public class Ammo : MonoBehaviour
     public void ReduceAmmoAmount(AmmoType ammoType)
     {
         GetAmmoSlot(ammoType).ammoAmount--;
-        
+
+    }
+    public void IncreaseAmmoAmount(AmmoType ammoType, int ammoAmount)
+    {
+        GetAmmoSlot(ammoType).ammoAmount += ammoAmount;
+
     }
     private AmmoSlot GetAmmoSlot(AmmoType ammoType)
     {
-        foreach(AmmoSlot slot in ammoSlots)
+        foreach (AmmoSlot slot in ammoSlots)
         {
-            if(slot.ammoType==ammoType)
+            if (slot.ammoType == ammoType)
             {
                 return slot;
             }
---
+++ Assets/Scripts/AmmoPickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickUp : MonoBehaviour
+{
+    [SerializeField] int ammoamount=5;
+    [SerializeField] AmmoType ammoType;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<Ammo>().IncreaseAmmoAmount(ammoType, ammoamount);
+            Destroy(gameObject);
+        }
+
+    }
+}
