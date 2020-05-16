38dccb8e68d9bcfc143224d1733518ca449182eb
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-05-16 11:10:22 -0400

    Added Flash Light
    +Takes time for flashlight to fade away

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light mylight;
+
+    private void Start()
+    {
+        mylight = GetComponent<Light>();
+    }
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    void DecreaseLightAngle()
+    {
+        if (mylight.spotAngle <= minimumAngle) return;
+        else
+        {
+            mylight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+
+
+    }
+    void DecreaseLightIntensity()
+    {
+        mylight.intensity -= lightDecay * Time.deltaTime;
+    }
+}
